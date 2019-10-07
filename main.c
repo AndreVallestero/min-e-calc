afec1dce664e2bb756d03d10a702efd700d7a7c6
Andre Vallestero <andrevallestero@gmail.com>
2019-10-06 20:25:58 -0400

    Added files for hello world

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define EFL_BETA_API_SUPPORT 1
+
+#include <Efl_Core.h>
+
+void efl_main(void *data EINA_UNUSED, const Efl_Event *ev EINA_UNUSED) {
+	printf("Hello, World!\n");
+	efl_exit(0);
+}
+
+EFL_MAIN()
